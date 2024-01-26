480834ecec1a44ac184cfd79de69b3583b68bee0
oMatheussch <matisschmidt@gmail.com>
2024-01-26 06:48:31 -0300

    Tables Import

--- python/import_files.py
+++ python/import_files.py
@@ -32,6 +32,46 @@ import_employee_territories = con.cursor()
 import_employee_territories.execute("select * from employee_territories")
 employee_territories_select = import_employee_territories.fetchall()
 
+#Importando Employee
+import_employee = con.cursor()
+import_employee.execute("select * from employees e")
+employee_select = import_employee.fetchall()
+
+#Importando Products
+import_products = con.cursor()
+import_products.execute("select * from products p ")
+products_select = import_products.fetchall()
+
+#Importando Employee Territories
+import_employee_territories = con.cursor()
+import_employee_territories.execute("select * from employee_territories")
+employee_territories_select = import_employee_territories.fetchall()
+
+#Importando Region
+import_region = con.cursor()
+import_region.execute("select * from region r ")
+region_select = import_region.fetchall()
+
+#Importando Shippers
+import_shippers = con.cursor()
+import_shippers.execute("select * from shippers s ")
+shippers_select = import_shippers.fetchall()
+
+#Importando Suppliers
+import_suppliers = con.cursor()
+import_suppliers.execute("select * from suppliers s")
+suppliers_select = import_suppliers.fetchall()
+
+#Importando Territories
+import_territories = con.cursor()
+import_territories.execute("select * from territories t")
+territories_select = import_territories.fetchall()
+
+#Importando US States
+import_us_states = con.cursor()
+import_us_states.execute("select * from us_states us")
+us_states_select = import_us_states.fetchall()
+
 #Exibição no console
 for res in orders_select:
     print (res)
