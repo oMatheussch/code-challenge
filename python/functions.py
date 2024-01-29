2e056b354d02368b45dce03c3e7252d4ea1260e1
oMatheussch <matisschmidt@gmail.com>
2024-01-29 13:26:46 -0300

    Alocando dados do DB localmente com a função date_function()

--- python/functions.py
+++ python/functions.py
@@ -6,3 +6,11 @@ def date_function():
     actual_data = data_time.date()
     return actual_data
 
+def alocate_data(name, df):
+    directory = f'local_data/{name}/'
+    if os.path.exists(directory):
+        print('Diretório Existente')
+        df.to_csv(f'{directory}{name}_{date_function()}.csv', index=False)
+    else: 
+        os.makedirs(directory) 
+        df.to_csv(f'{directory}{name}_{date_function()}.csv', index=False)   
--- python/import_db_files.py
+++ python/import_db_files.py
@@ -1,19 +1,22 @@
 from database_connection import db_connection
 import os
-from functions import date_function
+from functions import alocate_data
+import pandas as pd
 
 con = db_connection()
+
+import_tables = con.cursor()
+import_tables.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+tables_select = import_tables.fetchall()
+print(tables_select)
 
 #Importando Orders
 import_orders = con.cursor()
 import_orders.execute("SELECT * FROM orders")
-import_orders.description("SELECT * FROM orders")
+columns = [desc[0] for desc in import_orders.description]
 orders_select = import_orders.fetchall()
-print(orders_select)
-#directory = 'local_data/order_detail/'
-#archive_name = f'order_detail_{date_function()}.csv'
-#archive_path = os.path.join(directory, archive_name)
-
+#print(columns)
+#print(orders_select)
 
 #Importando Categorias
 import_categories = con.cursor()
@@ -23,7 +26,10 @@ categories_select = import_categories.fetchall()
 #Importando Customer Demo
 import_customers_demo = con.cursor()
 import_customers_demo.execute("select * from customer_customer_demo ccd")
+columns = [desc[0] for desc in import_orders.description]
 customer_demo_select = import_customers_demo.fetchall()
+#print(columns)
+#print(customer_demo_select)
 
 #Importando Customer Demographics
 import_customer_demographics = con.cursor()
@@ -43,7 +49,11 @@ employee_territories_select = import_employee_territories.fetchall()
 #Importando Employee
 import_employee = con.cursor()
 import_employee.execute("select * from employees e")
+employees_column = [desc[0] for desc in import_employee.description]
 employee_select = import_employee.fetchall()
+df_employee = pd.DataFrame(employee_select, columns=employees_column)
+alocate_data('employee', df_employee)
+
 
 #Importando Products
 import_products = con.cursor()
@@ -82,5 +92,4 @@ us_states_select = import_us_states.fetchall()
 
 #for orders in orders_select:
 #    print(orders)
-
 con.close()
